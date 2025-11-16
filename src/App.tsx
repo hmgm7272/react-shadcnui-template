8032bfaa2f7387b33a7ea4f623bb26e457d63582
hmgm7272 <yuart7272@gmail.com>
2025-11-16 15:51:51 +0900

    add full template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background">
+      <Button>shadcn/ui 動いた？</Button>
+    </div>
+  )
+}
+
+export default App
